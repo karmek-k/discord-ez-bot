216214ac0c706d9612f766b24b61197077811832
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-05-24 16:37:47 +0200

    Create a basic DiscordBotBuilder

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello, world!');
+import Discord from 'discord.js';
+
+/**
+ * The main bot builder.
+ */
+export class DiscordBotBuilder {
+  private prefix = '!';
+
+  /**
+   * Sets the bot's command prefix
+   * (default is `!`).
+   */
+  public setPrefix(prefix: string): DiscordBotBuilder {
+    this.prefix = prefix;
+
+    return this;
+  }
+
+  /**
+   * Builds a Discord.js's `Discord.Client` instance
+   * from given properties.
+   */
+  public build(): Discord.Client {
+    const bot = new Discord.Client();
+    
+    return bot;
+  }
+}
