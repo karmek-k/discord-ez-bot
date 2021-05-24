2d28a4fe74a5cd2d6b8a554d833a29aa26416a47
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-05-24 16:49:25 +0200

    Define `on` method, rename setPrefix to cmdPrefix

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
 import Discord from 'discord.js';
 
+type Callback = () => any;
+type Event = keyof Discord.ClientEvents;
+
 /**
  * The main bot builder.
  */
 export class DiscordBotBuilder {
   private prefix = '!';
 
+  private events: Map<Event, Callback> = new Map();
+
   /**
    * Sets the bot's command prefix
-   * (default is `!`).
+   * (the default one is `!`).
    */
-  public setPrefix(prefix: string): DiscordBotBuilder {
+  cmdPrefix(prefix: string): DiscordBotBuilder {
     this.prefix = prefix;
+
+    return this;
+  }
+
+  /**
+   * Defines the callback that is executed when the bot
+   * receives the given event.
+   */
+  on(event: Event, callback: Callback): DiscordBotBuilder {
+    this.events.set(event, callback);
 
     return this;
   }
@@ -20,8 +35,12 @@ export class DiscordBotBuilder {
    * Builds a Discord.js's `Discord.Client` instance
    * from given properties.
    */
-  public build(): Discord.Client {
+  build(): Discord.Client {
     const bot = new Discord.Client();
+
+    this.events.forEach((cb, e) => {
+      bot.on(e, cb);
+    });
     
     return bot;
   }
